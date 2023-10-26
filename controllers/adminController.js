add3183b285983c7dc0ea13937ef521126e1c3d6
Tifar <istighfar.amal@gmail.com>
2023-10-26 07:50:41 +0700

    delete assign role

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -89,19 +89,6 @@ exports.deleteUser = async (req, res) => {
   }
 };
 
-exports.assignRole = async (req, res) => {
-  const { id, role } = req.body;
-
-  try {
-    const user = await User.findByIdAndUpdate(id, { role }, { new: true });
-    res.status(200).json({ message: "succes", user });
-  } catch (error) {
-    res
-      .status(500)
-      .json({ error: "An error occurred while assigning the role" });
-  }
-};
-
 exports.updateProfile = async (req, res) => {
   const { username, email } = req.body;
   const id = req.user.id;
--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -6,6 +6,5 @@ router.post("/create-user", adminController.createUser);
 router.put("/update-user/:id", adminController.updateUser);
 router.delete("/delete-user/:id", adminController.deleteUser);
 router.get("/list-user", adminController.listUsers);
-router.post("/assign-role/", adminController.assignRole);
 
 module.exports = router;
