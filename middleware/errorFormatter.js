2d169e9474cf8b5e94df86eff421bd429a3e60af
Tifar <istighfar.amal@gmail.com>
2023-10-27 00:18:23 +0700

    add error formatter

--- middleware/errorFormatter.js
+++ middleware/errorFormatter.js
@@ -3,7 +3,7 @@ function errorFormatter(err, req, res, next) {
     return res.status(err.status || 400).json({
       error: err.message,
     });
-  }.
+  }
 
   if (err.error) {
     return res.status(err.status || 400).json({
