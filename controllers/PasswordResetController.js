0253c9fbbb760be971fc62aaa6e9bb6e9b78e38c
m-istighfar <istighfar.amal@gmail.com>
2023-12-07 17:44:58 +0700

    implement standart response in passwordreset con

--- controllers/PasswordResetController.js
+++ controllers/PasswordResetController.js
@@ -4,6 +4,14 @@ const bcrypt = require("bcrypt");
 const crypto = require("crypto");
 const { sendPasswordResetEmail } = require("../services/mailService");
 
+const successResponse = (res, message, data = null) => {
+  return res.status(200).json({ message, data });
+};
+
+const errorResponse = (res, message, statusCode = 400) => {
+  return res.status(statusCode).json({ error: message });
+};
+
 const requestPasswordReset = async (req, res) => {
   const { email } = req.body;
   const userAuth = await prisma.userAuth.findUnique({
@@ -11,9 +19,7 @@ const requestPasswordReset = async (req, res) => {
   });
 
   if (!userAuth) {
-    return res
-      .status(400)
-      .json({ error: "No account with that email address exists." });
+    return errorResponse(res, "No account with that email address exists.");
   }
 
   const resetToken = crypto.randomBytes(20).toString("hex");
@@ -30,12 +36,12 @@ const requestPasswordReset = async (req, res) => {
   const { success, error } = await sendPasswordResetEmail(email, resetToken);
 
   if (!success) {
-    return res
-      .status(500)
-      .json({ error: "Failed to send reset email.", details: error });
+    return errorResponse(res, "Failed to send reset email.", 500, {
+      details: error,
+    });
   }
 
-  res.status(200).json({ message: "Password reset email sent." });
+  return successResponse(res, "Password reset email sent.");
 };
 
 const resetPassword = async (req, res) => {
@@ -52,9 +58,10 @@ const resetPassword = async (req, res) => {
   });
 
   if (!userAuth) {
-    return res
-      .status(400)
-      .json({ error: "Password reset token is invalid or has expired." });
+    return errorResponse(
+      res,
+      "Password reset token is invalid or has expired."
+    );
   }
 
   try {
@@ -69,9 +76,9 @@ const resetPassword = async (req, res) => {
       },
     });
 
-    res.status(200).json({ message: "Password successfully reset." });
+    return successResponse(res, "Password successfully reset.");
   } catch (error) {
-    res.status(400).json({ error: error.message });
+    return errorResponse(res, error.message);
   }
 };
 
