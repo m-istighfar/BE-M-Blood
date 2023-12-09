729a4b9a22cfdf6289f0e37eccbfe99afccf738c
m-istighfar <istighfar.amal@gmail.com>
2023-12-09 17:56:36 +0700

    add pagination getall - blood drive

--- controllers/BloodDriveController.js
+++ controllers/BloodDriveController.js
@@ -3,14 +3,27 @@ const prisma = new PrismaClient();
 
 exports.getAllBloodDrives = async (req, res) => {
   try {
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10;
+    const offset = (page - 1) * limit;
+
     const bloodDrives = await prisma.bloodDrive.findMany({
+      skip: offset,
+      take: limit,
       include: {
         Province: true,
         User: true,
       },
     });
 
-    res.status(200).json(bloodDrives);
+    const totalRecords = await prisma.bloodDrive.count();
+
+    res.status(200).json({
+      totalRecords,
+      totalPages: Math.ceil(totalRecords / limit),
+      currentPage: page,
+      bloodDrives,
+    });
   } catch (error) {
     res.status(500).json({
       error: "An error occurred while fetching blood drives: " + error.message,
