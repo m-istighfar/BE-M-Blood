a086a0e3b97ebc608f3d18e663cf5cb891f994cb
m-istighfar <istighfar.amal@gmail.com>
2023-12-09 18:26:16 +0700

    update route - emergency

--- routes/emergencyRoutes.js
+++ routes/emergencyRoutes.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const router = express.Router();
 const EmergencyController = require("../controllers/EmergencyController");
-const { route } = require("./appointmentRoutes");
 
 router.post("/request", EmergencyController.createEmergencyRequest);
+
 router.get("/", EmergencyController.getAllEmergencyRequests);
+
 router.get("/:emergencyRequestId", EmergencyController.getEmergencyRequestById);
+
+router.put("/:emergencyRequestId", EmergencyController.updateEmergencyRequest);
+
+router.delete(
+  "/:emergencyRequestId",
+  EmergencyController.deleteEmergencyRequest
+);
+
 module.exports = router;
