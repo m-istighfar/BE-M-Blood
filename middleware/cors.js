5326acc75a94e22a83c7de673f023f893ed650fe
m-istighfar <istighfar.amal@gmail.com>
2023-12-22 17:42:30 +0700

    try deploy - set cors

--- middleware/cors.js
+++ middleware/cors.js
@@ -1,17 +1,20 @@
 const cors = require("cors");
 
 module.exports = (app) => {
-  // const allowedOrigins = ["http://localhost:5173", "http://127.0.0.1:5500"];
+  const allowedOrigins = [
+    "http://localhost:5173",
+    "https://fe-final-project-revou.vercel.app",
+  ];
 
-  // const corsOptions = {
-  //   origin: function (origin, callback) {
-  //     if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-  //       callback(null, true);
-  //     } else {
-  //       callback(new Error("Not allowed by CORS"));
-  //     }
-  //   },
-  // };
+  const corsOptions = {
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+  };
 
   app.use(cors());
 };
