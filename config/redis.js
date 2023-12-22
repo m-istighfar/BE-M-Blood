552248e8df9e739f8a82c78cc24bd72a9fa3de9e
m-istighfar <istighfar.amal@gmail.com>
2023-12-22 19:50:13 +0700

    try deploy - set cors

--- config/redis.js
+++ config/redis.js
@@ -8,7 +8,6 @@ const Redis = require("ioredis");
 const redisConfig = {
   host: process.env.REDIS_HOST,
   port: process.env.REDIS_PORT,
-  password: process.env.REDIS_PASSWORD,
 };
 const redis = new Redis(redisConfig);
 
