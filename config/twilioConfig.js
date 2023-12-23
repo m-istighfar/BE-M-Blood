b1f175b5a2e50dc77aefa408693fc31932dae948
m-istighfar <istighfar.amal@gmail.com>
2023-12-23 11:19:36 +0700

    ok cron

--- config/twilioConfig.js
+++ config/twilioConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
-  accountSid: process.env.TWILIO_ACCOUNT_SID, // Twilio Account SID
-  authToken: process.env.TWILIO_AUTH_TOKEN, // Twilio Auth Token
-  whatsappNumber: process.env.TWILIO_WHATSAPP_NUMBER, // Your Twilio WhatsApp number like 'whatsapp:+14155238886'
+  accountSid: process.env.TWILIO_ACCOUNT_SID,
+  authToken: process.env.TWILIO_AUTH_TOKEN,
+  whatsappNumber: process.env.TWILIO_WHATSAPP_NUMBER,
 };
